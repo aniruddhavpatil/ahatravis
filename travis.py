6b85ea9b1942d60084188af658a862913f329273
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-05-18 20:12:01 -0400

    Added travis script to list files diff with master

--- travis.py
+++ travis.py
@@ -1,14 +1,39 @@
 from git import Repo
+import re
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from dotenv import load_dotenv
+import os
 
 def getChangedFiles():
     repo = Repo('./')
     assert not repo.bare
-    changedFiles = [item.a_path for item in repo.index.diff(None)]
-    print(changedFiles)
+    diff = repo.git.diff('HEAD..' + os.getenv("TRAVIS_BRANCH"), name_only=True)
+    return diff.split('\n')
+
+def filterChangedFiles(changedFiles):
+    filteredFiles = []
+    regex = "^events/.*\.json$"
+    for file in changedFiles:
+        if re.search(regex, file):
+            filteredFiles.append(file)
+    return filteredFiles
+
+def createEvent(file):
+    cred = credentials.Certificate('serviceAccount.json')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    db.collection(u'events').document().set(event)
 
 def travis():
-    getChangedFiles()
+    changedFiles = getChangedFiles()
+    filteredFiles = filterChangedFiles(changedFiles)
+    print(filteredFiles)
+
 
 
 if __name__ == '__main__':
+    # load_dotenv()
+    # print(os.getenv("HELLO"))
     travis()
